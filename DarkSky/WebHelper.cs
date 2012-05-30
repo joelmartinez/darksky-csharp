469adfde9e9abcb90266d8b6ae82f8d9aea5ea61
Joel Martinez <joelmartinez@gmail.com>
2012-05-29 22:51:41 -0400

    Windows Phone version, modified WebHelper to use TaskCompletionSource

--- DarkSky/WebHelper.cs
+++ DarkSky/WebHelper.cs
@@ -10,24 +10,43 @@ namespace DarkSky
 	internal static class WebHelper
 	{
 		public static Task<T> Json<T>(string url)
-		{
-			return Task.Factory.StartNew(() => 
-			{
-				var request = WebRequest.CreateHttp(url);
-				
-				var response = request.GetResponse();
-				var sreader = new StreamReader(response.GetResponseStream());
-                var result = sreader.ReadToEnd();
-				 
-				DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
-                byte[] bytes = Encoding.UTF8.GetBytes(result);
-                using (var stream = new MemoryStream(bytes))
+        {
+            var tcs = new TaskCompletionSource<T>();
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            try
+            {
+                request.BeginGetResponse(iar =>
                 {
-                    var deserialized = serializer.ReadObject(stream);
+                    HttpWebResponse response = null;
+                    try
+                    {
+                        response = (HttpWebResponse)request.EndGetResponse(iar);
+                        if (response.StatusCode == HttpStatusCode.OK)
+                        {
+                            var sreader = new StreamReader(response.GetResponseStream());
+                            var result = sreader.ReadToEnd();
 
-                    return (T)deserialized;
-                }
-			});
+                            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+                            byte[] bytes = Encoding.UTF8.GetBytes(result);
+                            using (var stream = new MemoryStream(bytes))
+                            {
+                                var deserialized = serializer.ReadObject(stream);
+
+                                tcs.SetResult( (T)deserialized );
+                            }
+                        }
+                        else
+                        {
+                            tcs.SetResult(default(T));
+                        }
+                    }
+                    catch (Exception exc) { tcs.SetException(exc); }
+                    finally { if (response != null) response.Close(); }
+                }, null);
+            }
+            catch (Exception exc) { tcs.SetException(exc); }
+            return tcs.Task;
+
 		}
 	}
 }
